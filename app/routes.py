18221861757a2da0454029cf8ae1757679a4e2f2
Fabio Heitor <pires.h@escolar.ifrn.edu.br>
2025-11-13 14:30:09 -0300

    arquivos iniciais

---
+++ app/routes.py
@@ -0,0 +1,12 @@
+from app import app
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
+# ...
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # ...
+        return redirect(url_for('index'))
+    # ...
