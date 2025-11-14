9c001a8ddab89377a6fea59230099cf6868f7cf2
fabio <pires.h@escolar.ifrn.edu.br>
2025-11-14 01:54:52 -0300

    ;

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,8 @@
 from app import app
-from flask import render_template, flash, redirect, url_for
-from app.forms import LoginForm
-# ...
+from flask import render_template
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        # ...
-        return redirect(url_for('index'))
-    # ...
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'fabio'}
+    return render_template('index.html', title='Home', user=user)
